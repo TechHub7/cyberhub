ea86ff6c644ebe46fa4d6b4e2978e7a3ca5873ea
Chigoziem Christian <TechHub7>
2024-11-06 13:24:55 +0100

    Changed from SQL to Postgre SQL

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,12 +1,12 @@
 // const DB = "INSERT INTO `users` (`id`, `username`, `email`, `password`, `create_At`, `role`) VALUES (NULL, 'Apst. Abel', 'abel@gmail.com', MD5('123456'), current_timestamp(), 'User')";
 // const sql = require("mysql2/promise")
-const sql = require("mysql2")
+const sql = require("postgres")
 require('dotenv').config()
 const MYSQL = sql.createConnection({
-    host: process.env.HOST || "localhost",
-    user: process.env.USER || "root",
-    password: process.env.PASS || "",
-    database: process.env.DATABASE || "cyberhub",
+    host: process.env.platform == "DEVELOPMENT" ? process.env.HOST : process.env.POST_HOST,
+    user: process.env.platform == "DEVELOPMENT" ? process.env.USER : process.env.POST_USER,
+    password: process.env.platform == "DEVELOPMENT" ? process.env.PASS : process.env.POST_PASS,
+    database: process.env.platform == "DEVELOPMENT" ? process.env.DATABASE : process.env.POST_DATABASE,
 })
 
 // console.log(process.env.PWD);
